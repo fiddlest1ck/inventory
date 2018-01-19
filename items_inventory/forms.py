46876aaa2ac90ca6db73aa8b594ea74b9825a8ec
maciek <maciej.kuzma@cloudware.pl>
2018-01-19 13:47:42 +0100

    Nowakupa

--- items_inventory/forms.py
+++ items_inventory/forms.py
@@ -13,19 +13,19 @@ class RecordForm(forms.ModelForm):
         model = Record
         fields = '__all__'
 
-    def clean(self):
-        number_of_incoming = int(1 if None else self.cleaned_data['number_of_incoming'])
-        number_of_outgoing = int(self.cleaned_data['number_of_outgoing']) or 0
-        number_result = int(self.cleaned_data['number_result']) or 0
+    # def clean(self):
+    #     number_of_incoming = int(1 if None else self.cleaned_data['number_of_incoming'])
+    #     number_of_outgoing = int(self.cleaned_data['number_of_outgoing']) or 0
+    #     number_result = int(self.cleaned_data['number_result']) or 0
 
-        incoming_value = int(self.cleaned_data['incoming_value']) or 0
-        outgoing_value = int(self.cleaned_data['outgoing_value']) or 0
-        value_result = int(self.cleaned_data['value_result']) or 0
-        result = number_of_incoming - number_of_outgoing
-        result2 = incoming_value - outgoing_value
-        if number_result != result:
-            raise ValidationError('Nie zgadza się stan ilości {}'.format(number_result))
-        if value_result != result2:
-            raise ValidationError('Nie zgadza się stan wartości {}'.format(value_result))
-        return self.cleaned_data
+    #     incoming_value = int(self.cleaned_data['incoming_value']) or 0
+    #     outgoing_value = int(self.cleaned_data['outgoing_value']) or 0
+    #     value_result = int(self.cleaned_data['value_result']) or 0
+    #     result = number_of_incoming - number_of_outgoing
+    #     result2 = incoming_value - outgoing_value
+    #     if number_result != result:
+    #         raise ValidationError('Nie zgadza się stan ilości {}'.format(number_result))
+    #     if value_result != result2:
+    #         raise ValidationError('Nie zgadza się stan wartości {}'.format(value_result))
+    #     return self.cleaned_data
         
